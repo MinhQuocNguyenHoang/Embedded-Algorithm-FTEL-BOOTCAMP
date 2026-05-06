1ac952b7c7bbfffa7d7c5455a88468956d901697
Nguyen Hoang Minh Quoc <billine30082005@gmail.com>
2026-05-06 16:24:15 +0700

    Time: 0 ms (100%), Space: 10.5 MB (35.49%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize){
+
+    int max = candies[0];
+    
+    for(int i = 1; i < candiesSize; i++){
+        if(candies[i] > max){
+            max = candies[i];
+        }
+    }
+
+    bool* result = (bool*)malloc(sizeof(bool) * candiesSize);
+
+    for(int i = 0; i < candiesSize; i++){
+        if(candies[i] + extraCandies >= max){
+            result[i] = true;
+        } else {
+            result[i] = false;
+        }
+    }
+
+    *returnSize = candiesSize;
+    return result;
+}
