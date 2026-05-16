402f0b70b8d8a9c14f47f7f89e47ad369087902e
Nguyen Hoang Minh Quoc <billine30082005@gmail.com>
2026-05-16 12:25:21 +0700

    Time: 0 ms (100%), Space: 17 MB (94.26%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.c
@@ -0,0 +1,23 @@
+double findMaxAverage(int *nums, int numsSize, int k)
+{
+    double sum = 0;
+
+    for (int i = 0; i < k; i++)
+    {
+        sum += nums[i];
+    }
+
+    double maxSum = sum;
+
+    for (int i = k; i < numsSize; i++)
+    {
+        sum = sum + nums[i] - nums[i - k];
+
+        if (sum > maxSum)
+        {
+            maxSum = sum;
+        }
+    }
+
+    return maxSum / k;
+}
